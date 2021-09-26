1c7381dda2d57918e62c89cebfc23984c22ac94c
luisiitoDev <luisasandovalcode@gmail.com>
2021-09-26 17:55:30 -0600

    Add project files.

---
+++ AzureRedisLab/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Configuration;
+using StackExchange.Redis;
+using System;
+using System.IO;
+
+namespace AzureRedisLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                        .SetBasePath(Directory.GetCurrentDirectory())
+                        .AddJsonFile("appsettings.json")
+                        .Build();
+
+            var connectionString = config["CacheConnection"];
+
+            using (var cache = ConnectionMultiplexer.Connect(connectionString))
+            {
+                IDatabase db = cache.GetDatabase();
+                bool setValue = db.StringSet("test:key", "some value");
+                Console.WriteLine($"SET: {setValue}");
+
+                string value = db.StringGet("test:key");
+                Console.WriteLine(value);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
